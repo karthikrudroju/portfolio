2ad29e637a5eda6af54a548540f1de3556a9ab93
Rudroju Karthik <karthikrudroju@gmail.com>
2021-10-26 20:50:21 +0530

    make responsive part two

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,6 +5,11 @@ function NavBar() {
     return (
         <div id="navbar" className={styles.container}>
             <div className={styles.nav}>
+            <input type='checkbox' id="check" />
+                <label for="check">
+                    <div className={styles.btn}><i class="fas fa-bars fa-2x"></i></div>
+                    <div className={styles.cancel}><i class="fas fa-times fa-2x"></i></div>
+                </label>
                 <ul>
                     <li><a href="#contact" className={styles.link}>Contact</a></li>
                     <li><a href="#projects" className={styles.link}>Projects</a></li>
